f8f19efa1c639515cc599c22f5c9ce1ea57a7c6d
Vladislava-Ivanova <ivanovavladislavav@gmail.com>
2024-04-11 02:37:50 +0300

    added constants, favourites-view.js, gif-details-view.js and home-view.js implementation

---
+++ src/constants/constants.js
@@ -0,0 +1,17 @@
+export const HOME_VIEW = 'home';
+
+export const ABOUT_VIEW = 'about';
+
+export const DISPLAY_UPLOADED_VIEW = 'display uploaded gifs';
+
+export const UPLOAD_VIEW = 'upload gifs';
+
+export const FAVORITES_VIEW = 'favorite gif';
+
+export const SEARCH_VIEW = 'search gifs';
+
+export const TRENDING_VIEW = 'display trending gifs';
+
+export const GIF_DETAILS_VIEW = 'display gif details';
+
+export const CONTAINER_SELECTOR = '#container';
---
+++ src/views/home-view.js
@@ -0,0 +1,14 @@
+/**
+ * The home view of the GIPHY APP.
+ * @returns {string} The content for the home view.
+ */
+export const homeView = () => `
+<div id="home">
+  <div class="content">
+    <h1>Welcome to the GIPHY APP</h1>
+    <h2>Created by: Group 2</h2>
+    <h3>Date: 2024</h3>
+    <p>This is a platform to explore and enjoy trending gifs from around the web.</p>
+  </div>
+</div>
+`;
