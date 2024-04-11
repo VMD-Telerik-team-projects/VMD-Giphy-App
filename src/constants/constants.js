2ed3035fad85c183735f9ac73c6426bb79d0b569
Vladislava-Ivanova <ivanovavladislavav@gmail.com>
2024-04-11 10:24:31 +0300

    edited constants, helpers and navigation events

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,16 +2,16 @@ export const HOME_VIEW = 'home';
 
 export const ABOUT_VIEW = 'about';
 
-export const DISPLAY_UPLOADED_VIEW = 'display uploaded gifs';
+export const DISPLAY_UPLOADED_VIEW = 'display-uploaded';
 
-export const UPLOAD_VIEW = 'upload gifs';
+export const UPLOAD_VIEW = 'upload';
 
-export const FAVORITES_VIEW = 'favorite gif';
+export const FAVORITES_VIEW = 'favorites';
 
-export const SEARCH_VIEW = 'search gifs';
+export const SEARCH_VIEW = 'search';
 
-export const TRENDING_VIEW = 'display trending gifs';
+export const TRENDING_VIEW = 'trending';
 
-export const GIF_DETAILS_VIEW = 'display gif details';
+export const GIF_DETAILS_VIEW = 'details';
 
 export const CONTAINER_SELECTOR = '#container';
