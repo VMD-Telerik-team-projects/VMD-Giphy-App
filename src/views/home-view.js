841e3976fdd0b52426c41411aecf7f93780144dc
Vladislava-Ivanova <ivanovavladislavav@gmail.com>
2024-04-11 14:51:36 +0300

    small changes to css and the image in home-view

--- src/views/home-view.js
+++ src/views/home-view.js
@@ -5,7 +5,7 @@
 export const toHomeView = () => `
 <div id="home">
   <div class="content">
-  <img src="./src/img/R.jpg" alt="GIPHY Logo" style="width: 50%; max-width: 200px; display: block; margin: 0 auto; border: 2px solid black;">
+  <img src="./src/img/R.jpg" alt="GIPHY Logo" class="content-image">
     <h1>Welcome to the GIPHY APP</h1>
     <h2>Created by: Group 2</h2>
     <h3>Date: 2024</h3>
