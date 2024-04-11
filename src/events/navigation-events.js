857526fc4ba86a86b053524d9702e5df56ac56ea
deyantomov <deyan.tomov.a58@learn.telerikacademy.com>
2024-04-11 11:57:50 +0300

    Fixed merge conflict. Home page now properly displays.

--- src/events/navigation-events.js
+++ src/events/navigation-events.js
@@ -1,41 +1,49 @@
-import { CONTAINER_SELECTOR, HOME_VIEW, DISPLAY_UPLOADED_VIEW, UPLOAD_VIEW, FAVORITES_VIEW, SEARCH_VIEW, TRENDING_VIEW, GIF_DETAILS_VIEW } from '../common/constants.js';
+import { CONTAINER_SELECTOR, HOME } from '../common/constants.js';
 import { q, setActiveNav } from './helpers.js';
 import { gifDetailsView } from '../views/gif-details-view.js';
 import { favoritesView } from '../views/favorites-view.js';
-import { homeView } from '../views/home-view.js';
-import { fetchObjectFromServer } from '../api-access.js';
+import { toHomeView } from '../views/home-view.js';
+import { fetchObjectFromServer } from '../api/api-access.js';
 
 /**
  * loads the specified page.
  * @param {string} page - The page to be loaded.
  */
 export const loadPage = (page = '') => {
-    switch (page) {
-      case HOME_VIEW:
-        setActiveNav(HOME_VIEW);
-        return displayHome();
-      case DISPLAY_UPLOADED_VIEW:
-        setActiveNav(DISPLAY_UPLOADED_VIEW);
-        return displayUploadedGifs();
-      case UPLOAD_VIEW:
-        setActiveNav(UPLOAD_VIEW);
-        return displayUpload();
-      case FAVORITES_VIEW:
-        setActiveNav(FAVORITES_VIEW);
-        return displayFavorites();
-      case SEARCH_VIEW:
-        setActiveNav(SEARCH_VIEW);
-        return displaySearch();
-      case TRENDING_VIEW:
-        setActiveNav(TRENDING_VIEW);
-        return displayTrendingGifs();
-      case GIF_DETAILS_VIEW:
-        setActiveNav(GIF_DETAILS_VIEW);
-        return displayGifDetails();
-      default:
-        return null;
-    }
-  };
+  switch (page) {
+    case HOME:
+      setActiveNav(HOME);
+      return displayHome();
+    // case DISPLAY_UPLOADED_VIEW:
+    //   setActiveNav(DISPLAY_UPLOADED_VIEW);
+    //   return displayUploadedGifs();
+    // case UPLOAD_VIEW:
+    //   setActiveNav(UPLOAD_VIEW);
+    //   return displayUpload();
+    // case FAVORITES_VIEW:
+    //   setActiveNav(FAVORITES_VIEW);
+    //   return displayFavorites();
+    // case SEARCH_VIEW:
+    //   setActiveNav(SEARCH_VIEW);
+    //   return displaySearch();
+    // case TRENDING_VIEW:
+    //   setActiveNav(TRENDING_VIEW);
+    //   return displayTrendingGifs();
+    // case GIF_DETAILS_VIEW:
+    //   setActiveNav(GIF_DETAILS_VIEW);
+    //   return displayGifDetails();
+    // default:
+    //   return null;
+  }
+};
+  
+/**
+ * Displays the home view.
+ */
+export const displayHome = () => {
+  console.log('home');
+  q(CONTAINER_SELECTOR).innerHTML = toHomeView();
+};
 
 /**
  * Displays GIF details.
@@ -61,10 +69,3 @@ export const displayFavorites = async () => {
     console.error(error);
   }
 };
-
-/**
- * Displays the home view.
- */
-export const displayHome = () => {
-    q(CONTAINER_SELECTOR).innerHTML = homeView();
-  };
