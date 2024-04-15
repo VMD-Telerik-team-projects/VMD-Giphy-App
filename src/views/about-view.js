049b0fbe6898553c277a9772104ee76fc62c1d44
VladiIv <ivanovavladislavav@gmail.com>
2024-04-15 22:23:17 +0300

    added gifs, emojis & text to be displayed + some css

--- src/views/about-view.js
+++ src/views/about-view.js
@@ -8,7 +8,7 @@ export const toAboutView = () => `
 <div id="about">
   <div class="content">
   <img src="./src/img/bubbles.gif" alt="GIPHY Logo" class="content-image">
-    <h1 id="#topH1">Welcome to the GIPHY APP</h1>
+    <h1 id="#topH1">Welcome to the VMD GIPHY APP</h1>
     <h1>Created by: Group 2</h1>
     <h1>Date: 2024</h1>
     <button id="switch-theme" class="glow-on-hover">
--- src/views/upload-view.js
+++ src/views/upload-view.js
@@ -7,6 +7,7 @@
 export const toUploadView = () => `
 <form id="upload-form">
   <input type="file" accept="image/gif" id="gif-input">
-  <button type="submit" class="gif-upload-btn">Upload</button>
+  <button type="submit" class="gif-upload-btn">Upload ⬆️</button>
 </form>
+<img src="./src/img/green.webp" alt="hi" class="content-image-u"/>
 `;
