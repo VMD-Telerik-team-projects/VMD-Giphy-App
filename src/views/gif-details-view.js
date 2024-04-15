ae704b4b4a9a09d0ab5ac5d9d9bdf1fda325e1d8
MiroslavDermendzhiev <miroslav.der78@gmail.com>
2024-04-15 16:40:31 +0300

    keep only the necessary info on gif details

--- src/views/gif-details-view.js
+++ src/views/gif-details-view.js
@@ -11,12 +11,9 @@ export const gifDetailsView = async (gif) =>
     <img src="${gif.data.images.original.url}" alt="${gif.data.title}">
   </div>
   <div class="gif-info">
-    <p>ID: ${gif.data.id}</p>
     <p>Title: ${gif.data.title}</p>
-    <p>Rating: ${gif.data.rating}</p>
-    <p>Uploader: ${gif.data.user ? gif.data.user.username : 'Unknown'}</p>
+    <p>Uploaded by: ${gif.data.user ? gif.data.user.username : "Unknown"}</p>
     <p>Upload Date: ${gif.data.import_datetime}</p>
-    <p>Source: ${gif.data.source}</p>
   </div>
 </div>
 `;
